f9a5ba2f663e8c61dd2aed51d4ed956fd05c3205
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-16 08:21:03 -0300

    Alterações 1944

--- C/C-1944.c
+++ C/C-1944.c
@@ -1,13 +1,12 @@
 #include <stdio.h> 
-#include <stdlib.h>
+#include <stdlib.h> 
 
 typedef struct pilha { // guarda os nós da Pilha
-//   int info;
-  char letra;//letras da palavra
+  char letra; // letras da palavra
   struct pilha *prox;
 }Pilha;
 
-// função de inicialização: retorna uma Pilha vazia
+// função de inicialização -> retorna uma Pilha vazia
 void inicializa(Pilha **l)
 {
   *l = NULL;
@@ -15,10 +14,10 @@ void inicializa(Pilha **l)
 
 int vazia (Pilha* l)
 {
-  return (l == NULL); //retorna 1 se l for igual a null (vazia)
+  return (l == NULL); // retorna 1 se l for igual a null (vazia)
 }
 
-// nessa funcao para inserir na lista.
+// função para inserir na lista
 Pilha* push(Pilha *p, char v)
 {
   // cria uma pilha temporaria para fazer as insercoes
@@ -30,10 +29,8 @@ Pilha* push(Pilha *p, char v)
   printf("\n Adicionou a letra: ");
   temp->prox = p;
 
-  // coloca temp em p para retornar p, eu poderia retornar temp, mas assim fica mais legivel
+  // coloca temp em p para retornar p
   p = temp;
-  // print para teste, ver o ultimo valor de p
-  printf("%c \n", p->letra);
 
   return p;
 }
@@ -60,106 +57,94 @@ int pop(Pilha **p)
         free(q);
         // atribuindo q->prox na pilha real, substituindo o que ela ja tinha
         *p = temp;
-        // printf("\n %d \n", vazia(*p));
         return 0;
     }
 }
-// pesquisa por um valor v
-// int pesquisa(Pilha* p){
-//     // pede para ao usuario o valor para pesquisar
-//     int v;
-//     printf("digite um valor para pesquisar: ");
-//     scanf("%d",&v);
-//     // cria uma pilha temṕoraria
-//     Pilha* temp1 = p;
-//     printf(" \n procurando o valor %d \n",v);
-//     // se a pilha so tiver 1 elemento ele faz uma busca simples comparando esse elemento
-//     if(vazia(temp1->prox)==1){
-//       // compara o elemento com o primeiro valor da pilha
-//       if(temp1->info==v){
-//           printf("\n Achou o valor %d \n",temp1->info);
-//           return temp1->info;
-//         }
-//         // se ele nao for igual, a pilha nao tem esse valor
-//       else{
-//           printf("\n nao achou \n");
-//           return -1;
-//         }
-//     }
-//     // enquanto vazia retornar 0, se info for igual a oq o usuario digitar, ele achou o elemento, se nao, ele exclue esse elemento da lista temporaria e faz o if de novo, printando entrou toda vez que ele passar pelo else, para ficar melhor de visualizar 
-//       while(vazia(temp1) != 1){
-//         if(temp1->info==v){
-//           printf("\n Achou o valor %d \n",temp1->info);
-//           return temp1->info; 
-//         }
-//         else{
-//           printf("\n entrou no else \n");
-//           printf("\n temp1 = temp1->prox \n");
-//           printf("\n %d foi substituido por %d \n", temp1->info,temp1->prox->info);
 
-//           // pop(&temp1);
-//           temp1 = temp1->prox;
-//         }
-//       }
-
-//     printf("\n n achou nada otario \n");
-//     return -1;
-    
-// }
-
+// printa o valor do topo
 void printaPrimeiroValor (Pilha* l)
 {
-  printf("\n Primeiro valor é: %c \n", l->letra);
+  printf("\n Primeiro valor é: %c \n",l->letra);
 }
 
-// void printaSegundoValor (Pilha* l)
-// {
-//   printf("\n Segundo valor é: %d \n",l->prox->info);
-// }
-
+// mostra valores da pilha
 void imprimePilha(Pilha* p){
   Pilha* temp = p;
 
-  while(vazia(temp) == 1){
-    printf("\n %c \n" ,temp->letra);
+  while(vazia(temp) != 1){
+    printf("%c " ,temp->letra);
     temp = temp->prox;
   }
 }
 
+// conta as vitórias e dá um pop nos caracteres
+int verificaGanhador(Pilha* p){
+  Pilha* temp = p;
+  int count = 8;
+  int count2 = 0;
+  char letras[8];
+
+  while(count>0){
+    letras[count2] = temp->letra;
+    printf("%c " ,temp->letra);
+    temp = temp->prox;
+    count -= 1;
+    count2 += 1;
+  }
+  if(letras[0] == letras[7] && letras[1] == letras[6] && letras[2] == letras[5]
+  && letras[3] == letras[4]){
+    printf("\n ganhador \n");
+    count = 8;
+    while(count>0){
+      pop(&p);
+      count-=1;
+    }
+    return 1;
+  }
+  return 0;
+}
+
+Pilha* preenchePilha(Pilha* p){
+  if(vazia(p) == 1){
+    p=push(p,'F');
+    p=push(p,'A');
+    p=push(p,'C');
+    p=push(p,'E');
+  }
+  return p;
+}
+
 int main(void) {
   Pilha *l;
-  inicializa(&l); // passando o endereço do ponteiro
+  inicializa(&l); // estou passando o endereço do ponteiro
   
   int qtd;
   char l1,l2,l3,l4;
 
+  l = preenchePilha(l);
+  printf("\n");
+
+  imprimePilha(l);
+  printf("\n");
+
   printf("\n Quantas pessoas tem?");
   scanf("%d",&qtd);
-
+  int count = 0;
   while(qtd>0){
-    // scanf(" %c",&l1);
-    // push(l, l1);
-    // scanf(" %c",&l2);
-    // push(l, l2);
-    // scanf(" %c",&l3);
-    // push(l, l3);
-    // scanf(" %c",&l4);
-    // push(l, l4);
     scanf(" %c %c %c %c", &l1, &l2, &l3, &l4);
-
-    push(l, l1);
-    push(l, l2);
-    push(l, l3);
-    push(l, l4);
-
+   
+    l = push(l, l1);
+    l = push(l, l2);
+    l = push(l, l3);
+    l = push(l, l4);
     qtd = qtd-1;
+    if(verificaGanhador(l)==1){
+      count +=1;
+    }
+    l = preenchePilha(l);
   }
-
-  // printaPrimeiroValor(l);
-  // imprimePilha(l);
-  // pop(&l);
-  // pesquisa(l, 8);
-  // printf("\n status da pilha: %d \n", vazia(l));
-
-  // printf("\n %p \n", l); //valor de lista é NULL
+  printf("\n numero de vitorias: %d",count);
+  printf("\n");
+  imprimePilha(l);
+  printf("\n");
 }
