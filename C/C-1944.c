76e531be5e2714501f69d449d481f77db64c4edf
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-16 16:59:09 -0300

    Resolução 1944

--- C/C-1944.c
+++ C/C-1944.c
@@ -24,9 +24,6 @@ Pilha* push(Pilha *p, char v)
   Pilha* temp = (Pilha *)malloc(sizeof(Pilha));
   // coloca v dentro de letra
   temp->letra = v;
-
-
-  printf("\n Adicionou a letra: ");
   temp->prox = p;
 
   // coloca temp em p para retornar p
@@ -41,14 +38,11 @@ int pop(Pilha **p)
   // se a pilha esta vazia ele retorna -1, indicando que nao deu pra excluir
     if (vazia(*p) == 1)
     {
-        printf("\n Pilha está vazia. \n");
         return -1;
     }
   // cria uma pilha temporaria q e uma temp
     else
     {  
-        printf("\n Apagando ultimo valor. \n");
-
         Pilha* q = *p;
         Pilha *temp = q;
         // salvando a posicao prox que ele aponta na variavel temp
@@ -61,42 +55,25 @@ int pop(Pilha **p)
     }
 }
 
-// printa o valor do topo
-void printaPrimeiroValor (Pilha* l)
-{
-  printf("\n Primeiro valor é: %c \n",l->letra);
-}
-
-// mostra valores da pilha
-void imprimePilha(Pilha* p){
-  Pilha* temp = p;
-
-  while(vazia(temp) != 1){
-    printf("%c " ,temp->letra);
-    temp = temp->prox;
-  }
-}
 
 // conta as vitórias e dá um pop nos caracteres
-int verificaGanhador(Pilha* p){
-  Pilha* temp = p;
+int verificaGanhador(Pilha** p){
+  Pilha* temp = *p;
   int count = 8;
   int count2 = 0;
   char letras[8];
 
   while(count>0){
     letras[count2] = temp->letra;
-    printf("%c " ,temp->letra);
     temp = temp->prox;
     count -= 1;
     count2 += 1;
   }
   if(letras[0] == letras[7] && letras[1] == letras[6] && letras[2] == letras[5]
   && letras[3] == letras[4]){
-    printf("\n ganhador \n");
     count = 8;
     while(count>0){
-      pop(&p);
+      pop(p);
       count-=1;
     }
     return 1;
@@ -122,12 +99,8 @@ int main(void) {
   char l1,l2,l3,l4;
 
   l = preenchePilha(l);
-  printf("\n");
 
-  imprimePilha(l);
-  printf("\n");
 
-  printf("\n Quantas pessoas tem?");
   scanf("%d",&qtd);
   int count = 0;
   while(qtd>0){
@@ -138,13 +111,10 @@ int main(void) {
     l = push(l, l3);
     l = push(l, l4);
     qtd = qtd-1;
-    if(verificaGanhador(l)==1){ // número de vitórias
+    if(verificaGanhador(&l)==1){ // número de vitórias
       count +=1;
     }
     l = preenchePilha(l);
   }
-  printf("\n numero de vitorias: %d",count);
-  printf("\n");
-  imprimePilha(l); // verificando situação da pilha
-  printf("\n");
+    printf("\n%d\n",count);
 }
